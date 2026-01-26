96d9d5c47beaab9d557506d20641ac8eaa3bb4af
Vitor hugo <99295735+VitorGoulartt@users.noreply.github.com>
2026-01-26 19:36:58 +0000

    commit 1

---
+++ inscricao/src/main/java/metalab/inscricao/controller/ControllerStudent.java
@@ -0,0 +1,13 @@
+package metalab.inscricao.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("metalab")
+@CrossOrigin
+public class ControllerStudent {
+
+
+}
---
+++ inscricao/src/test/java/metalab/inscricao/InscricaoApplicationTests.java
@@ -0,0 +1,13 @@
+package metalab.inscricao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InscricaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
